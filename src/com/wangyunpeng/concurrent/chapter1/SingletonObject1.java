b8afe60183199d7dbbbf649bda33b2a87685a3d5
WangWilliams <1179574672@qq.com>
2020-03-17 17:25:14 +0800

    单例多线程问题

---
+++ src/com/wangyunpeng/concurrent/chapter1/SingletonObject1.java
@@ -0,0 +1,46 @@
+package com.wangyunpeng.concurrent.chapter1;
+
+public class SingletonObject1 {
+    private static final SingletonObject1 instance = new SingletonObject1();
+
+    private SingletonObject1(){
+        //
+    }
+
+    public SingletonObject1 getInstance() {
+         return instance;
+    }
+}
+
+
+class SingletonLazyObject{
+    private static volatile SingletonLazyObject instance = null;
+
+    public SingletonLazyObject() {
+    }
+
+    public SingletonLazyObject getInstance(){
+        if (instance == null) {                                     // 1 走到这里
+            instance = new SingletonLazyObject();                   // 2 走到这里 会导致生成两个对象
+        }
+        return SingletonLazyObject.instance;
+    }
+}
+
+class SingletonDoubleCheckObject{
+    private static SingletonDoubleCheckObject instance = null;
+
+    public SingletonDoubleCheckObject() {
+    }
+
+    public SingletonDoubleCheckObject getInstance(){
+        if (instance == null) {                                     //
+            synchronized (SingletonDoubleCheckObject.class){
+                if (instance == null) {
+                    instance = new SingletonDoubleCheckObject();
+                }
+            }
+        }
+        return SingletonDoubleCheckObject.instance;
+    }
+}
